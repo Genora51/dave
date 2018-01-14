160dbb5fd05637a8d2a567d40531495cc9a4833b
Unknown <geno.racklinasher@gmail.com>
2018-01-14 12:44:08 +0000

    Create daemon inheritable class

---
+++ dave/daemons.py
@@ -0,0 +1,41 @@
+from daemon import DaemonContext
+from daemon.pidfile import TimeoutPIDLockFile as LockFile
+import sys
+import os
+
+
+class Daemon(object):
+    """A Daemon class which Daemons can inherit from."""
+
+    stdin = stdout = pidpath = None
+
+    def run(self):
+        raise NotImplementedError
+
+    def _closectx(self):
+        """Shuts down the daemon and closes the DaemonContext."""
+        pid = self.ctx.pidfile.read_pid()
+        os.kill(pid, 15)  # SIGTERM
+        self.ctx.close()
+
+    def __call__(self):
+        """Starts/stops/restarts the daemon based on argv."""
+        if self.pidpath is None:
+            print("No valid PID path.")
+            return
+        self.ctx = DaemonContext(
+            stdin=sys.stdin,
+            stdout=sys.stdout,
+            pidfile=LockFile(self.pidpath)
+        )
+        if sys.argv[1] == "start":
+            with self.ctx:
+                self.run()
+        elif sys.argv[1] == "stop":
+            self._closectx()
+        elif sys.argv[1] == "restart":
+            self._closectx()
+            with self.ctx:
+                self.run()
+        else:
+            print("Unrecognised argument.")
