463fb8fc937814a18cd631f6ac9e4c216b75fc70
Geno Racklin Asher <geno.racklinasher@gmail.com>
2018-01-14 20:08:01 +0000

    Updated sanitiser
    Fixes issues with HTML Tags

--- dave/ui/js/sanitiser.js
+++ dave/ui/js/sanitiser.js
@@ -1,24 +1,8 @@
-var tagBody = '(?:[^"\'>]|"[^"]*"|\'[^\']*\')*';
-
-var tagOrComment = new RegExp(
-    '<(?:'
-    // Comment body.
-    + '!--(?:(?:-*[^->])*--+|-?)'
-    // Special "raw text" elements whose content should be elided.
-    + '|script\\b' + tagBody + '>[\\s\\S]*?</script\\s*'
-    + '|style\\b' + tagBody + '>[\\s\\S]*?</style\\s*'
-    // Regular name
-    + '|/?[a-z]'
-    + tagBody
-    + ')>',
-    'gi'
-);
-
-function removeTags(html) {
-    var oldHtml;
-    do {
-        oldHtml = html;
-        html = html.replace(tagOrComment, '');
-    } while (html !== oldHtml);
-    return html.replace(/</g, '&lt;');
-}
+function removeTags(unsafe) {
+    return unsafe
+         .replace(/&/g, "&amp;")
+         .replace(/</g, "&lt;")
+         .replace(/>/g, "&gt;")
+         .replace(/"/g, "&quot;")
+         .replace(/'/g, "&#039;");
+ }
