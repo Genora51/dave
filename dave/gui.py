09fffaf3d85216dfd178c7babe6a913038703b0c
Geno Racklin Asher <geno.racklinasher@gmail.com>
2018-01-21 20:15:11 +0000

    Show web server in window

--- dave/gui.py
+++ dave/gui.py
@@ -1,4 +1,5 @@
-from PyQt5 import QtWidgets
+from PyQt5 import QtWidgets, QtWebEngineWidgets
+from PyQt5.QtCore import QUrl
 
 
 def main():
@@ -16,6 +17,13 @@ def main():
 
     win.setLayout(layout)
     win.resize(450, 700)
+
+    # Add a QWebEngineView
+    view = QtWebEngineWidgets.QWebEngineView()
+    view.setUrl(QUrl("http://localhost:5128/"))
+
+    # Add the web view to the layout
+    layout.addWidget(view)
 
     # Run the app
     win.show()
