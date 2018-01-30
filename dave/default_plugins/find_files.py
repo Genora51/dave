99a38df9c3437c965afb9827c01594e4f1282977
Geno Racklin Asher <geno.racklinasher@gmail.com>
2018-01-30 21:31:01 +0000

    Open application

--- dave/default_plugins/find_files.py
+++ dave/default_plugins/find_files.py
@@ -1,9 +1,30 @@
+import subprocess
+from fuzzywuzzy import process, fuzz
+from os import path
+
+
+def base_no_ext(pth):
+    return path.splitext(path.basename(pth))[0]
+
+
 class AppOpener:
     def __init__(self, data):
         self.data = data
 
     def __iter__(self):
-        yield "msg; say", "Hello, World!"
+        proc = subprocess.Popen(
+            ['mdfind', '-onlyin', '/', 'kMDItemKind=="Application"'],
+            stdout=subprocess.PIPE
+        )
+        apps = proc.communicate()[0].decode("utf-8").splitlines()
+        apps_rem = list(map(base_no_ext, apps))
+        proc.terminate()
+        query = " ".join(t.orth_ for t in self.data["keywords"])
+        best_app = process.extractOne(query, apps_rem, scorer=fuzz.ratio)
+        if best_app is not None:
+            ba = apps[apps_rem.index(best_app[0])]
+            yield "msg; say", "Opening {}.".format(best_app[0])
+            subprocess.call(["open", ba])
 
 
 def setup(app):
