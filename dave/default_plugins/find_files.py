b58c9ea070955da0e99dd15f158b1e13bbf0a995
Geno Racklin Asher <geno.racklinasher@gmail.com>
2018-01-31 18:44:41 +0000

    Use default scorer with high cutoff

--- dave/default_plugins/find_files.py
+++ dave/default_plugins/find_files.py
@@ -20,7 +20,10 @@ class AppOpener:
         apps_rem = list(map(base_no_ext, apps))
         proc.terminate()
         query = " ".join(t.orth_ for t in self.data["keywords"])
-        best_app = process.extractOne(query, apps_rem, scorer=fuzz.ratio)
+        best_app = process.extractOne(
+            query, apps_rem,
+            score_cutoff=75
+        )
         if best_app is not None:
             ba = apps[apps_rem.index(best_app[0])]
             yield "msg; say", "Opening {}.".format(best_app[0])
