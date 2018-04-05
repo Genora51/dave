f69b962c5242fca5ea76047d269ef2cc69274412
Geno Racklin Asher <geno.racklinasher@gmail.com>
2018-04-05 13:54:43 +0100

    Change selected plugins to synch

--- dave/default_plugins/hello.py
+++ dave/default_plugins/hello.py
@@ -1,26 +1,22 @@
 import random
 
+aliases = ["hi", "hello", "hey"]
 
-class HelloSayer:
-    aliases = ["hi", "hello", "hey"]
 
-    def __init__(self, data):
-        self.data = data
-
-    async def __aiter__(self):
-        greeting = random.choice(self.aliases).capitalize()
-        # Sometimes add ',' and/or 'there'
+def say_hello(data):
+    greeting = random.choice(aliases).capitalize()
+    # Sometimes add ',' and/or 'there'
+    if random.getrandbits(1):
         if random.getrandbits(1):
-            if random.getrandbits(1):
-                greeting += ','
-            greeting += ' there'
-        # Choose between full stop and exclamation mark
-        if random.getrandbits(1):
-            greeting += '.'
-        else:
-            greeting += '!'
-        yield "msg; say", greeting
+            greeting += ','
+        greeting += ' there'
+    # Choose between full stop and exclamation mark
+    if random.getrandbits(1):
+        greeting += '.'
+    else:
+        greeting += '!'
+    yield "msg; say", greeting
 
 
 def setup(app):
-    app.register_aliases(HelloSayer.aliases, HelloSayer)
+    app.register_aliases(aliases, say_hello)
